2db8dc017f98d178bb251f5c8caeeb6dd129b372
yefee8 <yavuzefefindikli8@gmail.com>
2024-02-10 11:18:12 +0300

    fix: datatable text positions

--- src/components/dataTable.tsx
+++ src/components/dataTable.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 export default function DataTable({
   columns,
   rows,
@@ -8,8 +10,8 @@ export default function DataTable({
   return (
     <div className="w-full flex flex-col rounded-[6px] border border-[#383854]">
       <div className="w-full flex justify-between text-base py-2.5 px-6 overflow-x-auto font-bold gap-4 no-scrollbar rounded-t-[6px] bg-[#383854]">
-        {columns.map((column) => {
-          return <div style={{
+        {columns.map((column, index) => {
+          return <div className={cn(index === 2 ? "text-right" : index === 1 ? "text-center" : "text-left")} style={{
             width:`calc(100%/${columns.length})` 
           }}>{column}</div>;
         })}
@@ -19,8 +21,8 @@ export default function DataTable({
         {rows.map((rowItems: string[]) => {
           return (
             <div className="flex justify-between text-base py-2.5 px-6 gap-4 no-scrollbar font-bold">
-              {rowItems.map((row) => {
-                return <div style={{
+              {rowItems.map((row, index) => {
+                return <div className={cn(index === 2 ? "text-right" : index === 1 ? "text-center" : "text-left")} style={{
                   width:`calc(100%/${rowItems.length})`
                 }}>{row}</div>;
               })}
